9cd603c47018ac3a5193fe6f7f054ddce83bc7e0
Borche Jankovski <bokiscout@hotmail.com>
2016-06-01 23:33:45 +0200

    Debugging Local Database
    - Main() is put inside try() block to catch exceptions
    - Exceptions are thrown on each PC deferent from the one I wrote the
    program onto.

--- project-management-and-notes/project-management-and-notes/Program.cs
+++ project-management-and-notes/project-management-and-notes/Program.cs
@@ -14,9 +14,15 @@ namespace project_management_and_notes
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            catch(Exception e){
+                MessageBox.Show(e.ToString());
+            }
         }
     }
 }
